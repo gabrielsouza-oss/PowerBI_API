5f1ce67ee7c64ea94306a1ad55d56d5f2a73825e
Gabriel de Oliveira Souza <64101105+gabrielsouza-oss@users.noreply.github.com>
2021-12-21 23:50:18 -0300

    Add files via upload

---
+++ PowerBI.ps1
@@ -0,0 +1,5 @@
+﻿Install-Module -Name MicrosoftPowerBIMgmt -Scope CurrentUser
+Login-PowerBI
+$result = Invoke-PowerBIRestMethod -Url "https://api.powerbi.com/v1.0/myorg/datasets/46cd2d67-22e7-4029-b29a-1c1d57928a52/refreshes?$top=1" -Method Get
+
+$wokspaceContents = $result | ConvertFrom-Json
